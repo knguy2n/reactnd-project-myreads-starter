daa6cf55ad96fb04dbc515b9a2488d27f8f1c2f4
Kien Nguyen <kdot@Kiens-MacBook-Pro.local>
2018-09-13 19:22:38 -0700

    add SearchBox component and Title component to searchPage

--- src/searchPage.js
+++ src/searchPage.js
@@ -1,36 +1,25 @@
 import React, {Component} from 'react'
 import sortBy from 'sort-by'
+import SearchBox from './searchBox.js'
+import Title from './title.js'
 
-class Searchpage extends Component {
-	state = {
-		showSearchPage: 
-	}
-
+class SearchPage extends Component {
+	state = {}
 	render() {
 		return(
-			<div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
+			<div className="app">
+				<Title/>
+				<SearchBox/>
+			</div>
+			
 
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
 
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+
 			)
 	}
 
+
+
 }
 
-export default Searchpage
+export default SearchPage
