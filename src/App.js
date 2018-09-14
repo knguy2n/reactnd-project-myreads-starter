b26940c60673f5be469726998c89acc48a73d6af
Kien Nguyen <kdot@Kiens-MacBook-Pro.local>
2018-09-13 19:21:24 -0700

    add import to various components

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import * as BooksAPI from './BooksAPI'
 import './App.css'
 import Title from './title.js'
 import escapeRegExp from 'escape-string-regexp' // need to copy over to search comp
-import sortBy from 'sort-by'  //Need to copy over to Results
+import SearchPage from './searchPage.js'
+
 
 class BooksApp extends React.Component {
   state = {
@@ -13,33 +14,25 @@ class BooksApp extends React.Component {
      * users can use the browser's back and forward buttons to navigate between
      * pages, as well as provide a good URL they can bookmark and share.
      */
-    showSearchPage: false
+    showSearchPage: false,
+    allBooks:[]
   }
+
+  componentDidMount() {
+  BooksAPI.getAll().then((allBooks) => {
+    this.setState({ allBooks })
+  })
+}
+
+
+
 
   render() {
     return (
       <div className="app">
         {this.state.showSearchPage ? (
-          <div className="search-books">
-            <div className="search-books-bar">
-              <a className="close-search" onClick={() => this.setState({ showSearchPage: false })}>Close</a>
-              <div className="search-books-input-wrapper">
-                {/*
-                  NOTES: The search from BooksAPI is limited to a particular set of search terms.
-                  You can find these search terms here:
-                  https://github.com/udacity/reactnd-project-myreads-starter/blob/master/SEARCH_TERMS.md
-
-                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
-                  you don't find a specific author or title. Every search is limited by search terms.
-                */}
-                <input type="text" placeholder="Search by title or author"/>
-
-              </div>
-            </div>
-            <div className="search-books-results">
-              <ol className="books-grid"></ol>
-            </div>
-          </div>
+          <SearchPage/>
+          
         ) : (
           <div className="list-books"> 
             <Title/> 
